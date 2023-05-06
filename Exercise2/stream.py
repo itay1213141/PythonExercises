13d551a3bdb70f2b775ad395c1287f6f06c51682
Itay Habani <itayhabani1213141@gmail.com>
2023-05-07 00:07:59 +0300

    Fixed the thread lock issue, now theres another one

--- Exercise2/stream.py
+++ Exercise2/stream.py
@@ -1,11 +1,12 @@
 import threading
 
+
 class Stream:
     def __init__(self):
         self.list = []
         self.action = None
         self.stopped = False
-        
+
         self.lock = threading.Lock()
         self.thread = threading.Thread(target=self.run)
         self.thread.start()
@@ -17,8 +18,7 @@ class Stream:
                     self.action()
                 elif not self.list:
                     self.action = None
-            
-    
+
     def add(self, item):
         with self.lock:
             self.list.append(item)
@@ -26,38 +26,41 @@ class Stream:
     def forEach(self, func):
         def action():
             while not self.stopped:
-                print(self.lock)
                 with self.lock:
-                    print('yo')
                     if self.list:
                         item = self.list.pop(0)
-                        print('popped', item)                        
-                        func(item)            
-                
+                    else:
+                        continue
+
+                func(item)
+
         self.action = action
 
     def apply(self, func):
         new_stream = Stream()
-        
+
         def action():
             while not self.stopped:
                 with self.lock:
                     if self.list:
                         item = self.list.pop(0)
-                        result = func(item)
-                        
-                        if result is True:
-                            new_stream.add(item)
-                        elif result is not None:
-                            new_stream.add(result)
-                            
+                    else:
+                        continue
+
+                    result = func(item)
+
+                    if result is True:
+                        new_stream.add(item)
+                    elif result is not None:
+                        new_stream.add(result)
+
         self.action = action
         return new_stream
 
     def stop(self):
         self.stopped = True
         self.thread.join()
-        
+
 
 s = Stream()
 
